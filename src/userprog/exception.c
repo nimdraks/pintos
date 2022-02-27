8453b630d1e8603b158b042d805bc19407ab6085
nimdrak <nimdrak@naver.com>
2022-02-27 19:32:13 +0900

    finish bad case

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -148,6 +148,11 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
+	if(user){
+		exit_unexpectedly(thread_current());
+		return;
+	}	
+
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
