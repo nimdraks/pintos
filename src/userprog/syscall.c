ea0e7e5c27bb41bfe10345c75050e1969a36b519
nimdrak <nimdrak@naver.com>
2023-06-25 15:50:20 +0900

    P3-1 Fix tempo

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -220,7 +220,7 @@ syscall_handler (struct intr_frame *f)
 				exit_unexpectedly(t);
 				return;
 			}
-
+/*
 			for(i=0; i<=fileSize; i++) {
 				a=*((char*)fileBuffer + i);
 				a++;
@@ -229,7 +229,8 @@ syscall_handler (struct intr_frame *f)
 			for (k=0; k<fileSize; k++){
 				fileBuffer[k]=0;
 			}
-
+	*/
+			memset(fileBuffer, 0, fileSize);
 //			printf("start to sys read\n");
 			f->eax = file_read(file, (void*)fileBuffer, fileSize);
 			printf("%d: finish to sysread\n", thread_tid());
