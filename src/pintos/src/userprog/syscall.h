43c3477e7a1ea6fb4c748318a9113fc2a1cdd46e
nimdrak <nimdrak@naver.com>
2023-07-25 17:17:09 +0900

    P4-1 Fix the malfunction of git

---
+++ src/pintos/src/userprog/syscall.h
@@ -0,0 +1,10 @@
+#ifndef USERPROG_SYSCALL_H
+#define USERPROG_SYSCALL_H
+#include "threads/thread.h"
+
+void syscall_init (void);
+bool check_ptr_invalidity(struct thread* t, void* ptr);
+void exit_unexpectedly(struct thread* t);
+void exit_expectedly(struct thread* t, int);
+
+#endif /* userprog/syscall.h */
